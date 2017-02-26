aca8c000a015049f2fd80fe5248fe1721e5247b7
Jim Padilla and Margie Nevarez <jim+margie@>
2017-02-25 16:02:35 -0800

    rails helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -60,5 +60,7 @@ VCR.configure do |c|
   c.cassette_library_dir = 'spec/cassettes'
   c.hook_into :webmock
   c.configure_rspec_metadata!
-  c.filter_sensitive_data('<twilio account sid>') { ENV['TWILIO_ACCOUNT_SID'] } # example for filtering data
+  c.filter_sensitive_data('<twilio account sid>') { ENV['TWILIO_ACCOUNT_SID'] }
+  c.filter_sensitive_data('<twilio auth token>') { ENV['TWILIO_AUTH_TOKEN'] }
+   # example for filtering data
 end
